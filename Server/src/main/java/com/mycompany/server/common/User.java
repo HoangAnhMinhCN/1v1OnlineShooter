ef65fd49e6ab87749ff6177b56514ac6ba4d3b75
TrgMSon <truongminhson1810@gmail.com>
2026-08-25 08:11:19 +0700

    add game lobby

---
+++ Server/src/main/java/com/mycompany/server/common/User.java
@@ -0,0 +1,14 @@
+package com.mycompany.server.common;
+
+public class User {
+    private String userName;
+    private String password;
+    private int id;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    
+}
