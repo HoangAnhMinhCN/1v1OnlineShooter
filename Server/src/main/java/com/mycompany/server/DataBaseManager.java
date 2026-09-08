74a4c295b9ebcf55fcf87fb7fa6758745f565559
HoangAnhMinhCN <b23dccn544hoanganhminh@gmail.com>
2026-09-08 15:44:10 +0700

    fix

--- Server/src/main/java/com/mycompany/server/DataBaseManager.java
+++ Server/src/main/java/com/mycompany/server/DataBaseManager.java
@@ -8,9 +8,10 @@ public class DataBaseManager {
     private static final String URL =
             "jdbc:mysql://localhost:3306/mydatabase";
     private static final String USER = "root";
-    private static final String PASSWORD = "your_password";
+    private static final String PASSWORD = "123456789";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+    
 }
