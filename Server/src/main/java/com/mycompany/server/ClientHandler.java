66f1fe7863b7ce3cd1e899da03d65fa2598eb976
HoangAnhMinhCN <b23dccn544hoanganhminh@gmail.com>
2026-09-09 21:49:19 +0700

    database và login đơn giản

--- Server/src/main/java/com/mycompany/server/ClientHandler.java
+++ Server/src/main/java/com/mycompany/server/ClientHandler.java
@@ -1,7 +1,8 @@
 package com.mycompany.server;
 
-import java.nio.channels.Channel;
-
+import java.sql.*;
+import java.nio.channels.*;
+import java.nio.ByteBuffer;
 public class ClientHandler {
 
     public static void handleClientPacket(String message, Channel clientChannel) {
@@ -11,11 +12,12 @@ public class ClientHandler {
             String type = parts[0];
             switch (type) {
                 case "LOGIN":
-                    login();
                     // Xử lý đăng nhập
+                    login(parts, clientChannel);
+                    
                     break;
                 case "REQUEST_MATCH":
-                    // Xu ly yêu cầu match
+                    // Xử lý yêu cầu match
                     break;
                 case "CHAT":
                     // Xử lý tin nhắn chat
@@ -29,8 +31,40 @@ public class ClientHandler {
         }
 
     } 
-    private static void login() {
+    private static void login(String[] parts, Channel clientChannel) {
         // Xử lý đăng nhập
-        // Connection connectionManager = DataBaseManager.getConnection();
+        String username1 = parts[1];
+        String password2 = parts[2];
+        System.out.println("Đăng nhập: " + username1 + ", Mật khẩu: " + password2);
+        try {
+            Connection connection = DataBaseManager.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM player where name = ? AND password = ?");
+            statement.setString(1, username1);
+            statement.setString(2, password2);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                // Đăng nhập thành công
+                System.out.println("login successed: " + username1);
+                sendTcpResponse("LOGIN_SUCCESS", clientChannel);
+            } else {
+                // Đăng nhập thất bại
+                System.out.println("Login failed" + username1);
+                sendTcpResponse("LOGIN_FAILED", clientChannel);
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void sendTcpResponse(String response, Channel clientChannel) {
+        // Gửi phản hồi về client qua TCP
+        if (clientChannel instanceof SocketChannel) {
+            SocketChannel socketChannel = (SocketChannel) clientChannel;
+            try {
+                socketChannel.write(ByteBuffer.wrap((response + "\n").getBytes()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
