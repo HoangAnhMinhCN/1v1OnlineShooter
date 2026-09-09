511c46164aa8987151d7ad82f15860ce0bb3a1bf
HoangAnhMinhCN <b23dccn544hoanganhminh@gmail.com>
2026-09-09 21:57:20 +0700

    ServerHandler
    class xử lý các gói tin nhận được từ servẻ

---
+++ Client/src/main/java/com/mycompany/client/ServerHandler.java
@@ -0,0 +1,31 @@
+package com.mycompany.client;
+
+import java.nio.channels.Channel;
+
+public class ServerHandler {
+    public static void handleServerPacket(String message, Channel clientChannel) {
+        
+        try {
+            String[] parts = message.split("\\|");
+            String type = parts[0];
+            switch (type) {
+                case "LOGIN":
+                    
+                    
+                    break;
+                case "value1":
+                   
+                    break;
+                case "value2":
+            
+                    break;
+
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    } 
+}
