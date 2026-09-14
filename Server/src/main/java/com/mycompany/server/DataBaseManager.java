b6f90f0f03884cb4e81913985bffb0b19532febd
hopdoanit <hopnd.b23cn350@stu.ptit.edu.vn>
2026-09-14 15:30:13 +0700

    feat: cuyển login sucess sang sảnh chờ, hiện thị thông báo khi đăng nhập thất bại

--- Server/src/main/java/com/mycompany/server/DataBaseManager.java
+++ Server/src/main/java/com/mycompany/server/DataBaseManager.java
@@ -4,10 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DataBaseManager {
-    private static final String URL =
-            "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/server_db";
-    private static final String USER = "zcwFYPLk7RCg8CJ.root";
-    private static final String PASSWORD = "kIYKKp2bCqIxdYOI";
+    private static final String URL = "jdbc:mysql://localhost:3306/server_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456789";
 
     public static Connection getConnection() {
         Connection conn = null;
@@ -15,8 +14,8 @@ public class DataBaseManager {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
             e.printStackTrace();
-        } 
+        }
         return conn;
     }
-    
+
 }
